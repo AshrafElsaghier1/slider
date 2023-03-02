74546f3021e3941f9cbd17040c6c3a54d7f06ef5
AshrafElsaghier1 <ashrafelsaghier17@gmail.com>
2023-03-02 02:11:55 +0200

    large screen slider

--- src/App.js
+++ src/App.js
@@ -1,97 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
 import { ImArrowRight, ImArrowLeft } from 'react-icons/im';
 import "./App.css"
-const imgs = [
-  {
-    id: 1,
-    imgSrc: "assets/imgs/banner.webp",
-    title: "Mercato"
-  },
-  {
-    id: 2,
-    imgSrc: "/assets/imgs/banner-2.webp", title: "Mercato"
-
-  },
-  {
-    id: 3,
-    imgSrc: "/assets/imgs/banner-3.webp", title: "Cut"
-
-  },
-  {
-    id: 4,
-    imgSrc: "/assets/imgs/banner-4.webp", title: "Eksel"
-
-  },
-  {
-    id: 5,
-    imgSrc: "/assets/imgs/banner-5.webp", title: "Taakkad"
-
-  },
-  {
-    id: 6,
-    imgSrc: "/assets/imgs/banner-6.webp", title: "Shiftat"
-
-  },
-  {
-    id: 7,
-    imgSrc: "/assets/imgs/banner-7.webp", title: "Salonat"
-
-  },
-  {
-    id: 8,
-    imgSrc: "/assets/imgs/banner-8.webp", title: "Kilinikat"
-
-  }, {
-    id: 9,
-    imgSrc: "/assets/imgs/banner-9.webp", title: "Basket"
-
-  }, {
-    id: 10,
-    imgSrc: "/assets/imgs/banner-10.webp", title: "A R2yk"
-
-  }, {
-    id: 11,
-    imgSrc: "/assets/imgs/banner-11.webp", title: "Mednza"
-
-  }, {
-    id: 12,
-    imgSrc: "/assets/imgs/banner-12.webp", title: "Zabon"
-
-  }, {
-    id: 13,
-    imgSrc: "/assets/imgs/banner-13.webp", title: "Bahaar"
-
-  }, {
-    id: 14,
-    imgSrc: "/assets/imgs/banner-14.webp", title: "Crepe & Waffle"
-
-  }, {
-    id: 15,
-    imgSrc: "/assets/imgs/banner-15.webp", title: "Al Yusr"
-
-  }
-];
+import imgs from "./dummyData"
 
 const App = () => {
-
-  let [currentImg, setCurrentImg] = useState(1);
-
   const elementSidebarRef = useRef(null);
   const elementActiveImg = useRef(null);
-  let [transformTranslate, setTransformTranslate] = useState({
+  const [transformTranslate, setTransformTranslate] = useState({
     transformMainImg: 0,
     gap: 0,
     transformSidebarImg: 0,
   })
+  const [currentImg, setCurrentImg] = useState(0);
   const { transformMainImg, gap, transformSidebarImg } = transformTranslate;
 
 
 
-  const activeHandler = (id) => {
-    setCurrentImg(id)
+  const sliderShowHandler = (id, index) => {
 
-    if (id > 1) {
+    if (id > index) {
       setTransformTranslate(prevState => {
         return {
           ...prevState,
@@ -108,7 +36,8 @@ const App = () => {
         }
       })
     }
-    if (id <= 4) {
+
+    if (id <= 3) {
       setTransformTranslate(prevState => {
         return {
           ...prevState,
@@ -116,123 +45,67 @@ const App = () => {
         }
       })
     }
-    if (id >= 5 && id < imgs.length - 1) {
+    if (id >= 4 && id < imgs.length) {
       setTransformTranslate(prevState => {
         return {
           ...prevState,
-          transformSidebarImg: (parseInt((id - 4) * (elementSidebarRef?.current?.offsetHeight + 10))),
+          transformSidebarImg: (parseInt((id - 3) * (elementSidebarRef?.current?.offsetHeight + 10))),
         }
       })
+      if (id === imgs.length - 1) {
+        setTransformTranslate(prevState => {
+          return {
+            ...prevState,
+            transformSidebarImg: (parseInt((id - 4) * (elementSidebarRef?.current?.offsetHeight + 10))),
+          }
+        })
+      }
     }
   }
 
+  const activeClickHandler = (id) => {
+    setCurrentImg(id - 1)
+    sliderShowHandler(id, 1)
+  }
 
   const nextArrowHandler = (id) => {
-    if (id >= 1) {
+    if (id >= 0) {
       setCurrentImg((prev) => prev + 1)
     }
-
-
-    if (id >= 1) {
-      setTransformTranslate(prevState => {
-        return {
-          ...prevState,
-          transformMainImg: parseInt(id) * parseInt(elementActiveImg?.current?.offsetWidth),
-          gap: 0,
-        }
-      })
-    } else {
-      setTransformTranslate(prevState => {
-        return {
-          ...prevState,
-          transformMainImg: 0,
-          gap: 0,
-        }
-      })
-    }
-    if (id <= 4) {
-      setTransformTranslate(prevState => {
-        return {
-          ...prevState,
-          transformSidebarImg: 0,
-        }
-      })
-    }
-    if (id >= 5 && id < imgs.length - 1) {
-      setTransformTranslate(prevState => {
-        return {
-          ...prevState,
-          transformSidebarImg: (parseInt((id - 4) * (elementSidebarRef?.current?.offsetHeight + 10))),
-        }
-      })
-    }
-
+    // 1 for async state
+    sliderShowHandler(id + 1, 0)
   }
-  // const prevArrowHandler = (id) => {
-  //   if (id <= imgs.length - 1) {
-  //     setCurrentImg(currentImg - 1)
-  //   }
-  //   if (id > 1) {
-  //     setTransformTranslate(prevState => {
-  //       return {
-  //         ...prevState,
-  //         transformMainImg: parseInt(id - 1) * parseInt(elementActiveImg?.current?.offsetWidth),
-  //         gap: 0,
-  //       }
-  //     })
-  //   } else {
-  //     setTransformTranslate(prevState => {
-  //       return {
-  //         ...prevState,
-  //         transformMainImg: 0,
-  //         gap: 0,
-  //       }
-  //     })
-  //   }
-  //   if (id <= 4) {
-  //     setTransformTranslate(prevState => {
-  //       return {
-  //         ...prevState,
-  //         transformSidebarImg: 0,
-  //       }
-  //     })
-  //   }
-  //   if (id >= 5 && id < imgs.length - 1) {
-  //     setTransformTranslate(prevState => {
-  //       return {
-  //         ...prevState,
-  //         transformSidebarImg: (parseInt((id - 4) * (elementSidebarRef?.current?.offsetHeight + 10))),
-  //       }
-  //     })
-  //   }
-  // }
-
-
-
+  const prevArrowHandler = (id) => {
+    if (id <= imgs.length - 1) {
+      setCurrentImg(currentImg - 1)
+    }
+    sliderShowHandler(id, 0)
+  }
 
   return (
     <div className='my-5' id='slider'>
       <Container>
         <Row className='align-items-center align-items-lg-start'>
-          <Col md={3} lg={2} className=' py-0 '>
-            <div className='overflow-hidden  '>
-              <div className='sidebar-container py-0 d-flex flex-column   ' style={{ transform: `translateY(-${transformSidebarImg}px)` }} >
+
+          <Col md={3} lg={2} className=' py-0 order-2 order-md-1  '>
+            <div className='overflow-hidden  '  >
+              <div className='sidebar-container py-0 d-flex flex-row flex-md-column' style={{ transform: `translateY(-${transformSidebarImg}px)` }} >
                 {imgs.map(({ imgSrc, id }) => (
-                  <div key={id} className={`imgs-sidebar-container ${currentImg === id ? "active" : ""} `}
+                  <div key={id} className={`imgs-sidebar-container ${currentImg + 1 === id ? "active" : ""} `}
                     ref={elementSidebarRef}
-                    onClick={() => activeHandler(id)} >
+                    onClick={() => activeClickHandler(id)} >
                     <img src={imgSrc} alt={`img-${id}`} className="w-100" draggable={false} />
                   </div>
                 ))}
               </div>
             </div>
             <div className='btns-navigator d-flex align-items-center justify-content-center  '>
-              <button className='btn btn-sm' disabled={currentImg === 1 ? true : false}  > <ImArrowLeft />  </button>
+              <button className='btn btn-sm' disabled={currentImg === 0 ? true : false} onClick={() => prevArrowHandler(currentImg)}> <ImArrowLeft />  </button>
               <p className='mb-0'> {imgs[currentImg]?.title} </p>
-              <button className='btn btn-sm' disabled={currentImg === imgs.length - 1 ? true : false} onClick={() => nextArrowHandler(currentImg)} > <ImArrowRight /></button>
+              <button className='btn btn-sm' disabled={currentImg === imgs.length - 1 ? true : false} onClick={() => nextArrowHandler(currentImg + 1)} > <ImArrowRight /></button>
             </div>
           </Col>
-          <Col md={9} lg={10} className="mx-auto">
+          <Col md={9} lg={10} className="mx-auto  order-1 order-md-2 mb-5">
             <div className='overflow-hidden ' >
               <div className={`imgs-container d-flex h-100`} style={{ transform: `translateX(-${transformMainImg}px)`, gap: `${gap}` }} >
                 {imgs.map(({ imgSrc, id }) => (
---
+++ src/dummyData.js
@@ -0,0 +1,67 @@
+const imgs = [
+    {
+        id: 1,
+        imgSrc: "assets/imgs/banner.webp",
+        title: "Mercato"
+    },
+    {
+        id: 2,
+        imgSrc: "/assets/imgs/banner-3.webp",
+        title: "Cut"
+    },
+    {
+        id: 3,
+        imgSrc: "/assets/imgs/banner-4.webp",
+        title: "Eksel"
+    },
+
+    {
+        id: 4,
+        imgSrc: "/assets/imgs/banner-5.webp", title: "Taakkad"
+    },
+    {
+        id: 5,
+        imgSrc: "/assets/imgs/banner-6.webp", title: "Shiftat"
+
+    },
+    {
+        id: 6,
+        imgSrc: "/assets/imgs/banner-7.webp", title: "Salonat"
+
+    },
+    {
+        id: 7,
+        imgSrc: "/assets/imgs/banner-8.webp", title: "Kilinikat"
+
+    }, {
+        id: 8,
+        imgSrc: "/assets/imgs/banner-9.webp", title: "Basket"
+
+    }, {
+        id: 9,
+        imgSrc: "/assets/imgs/banner-10.webp", title: "A R2yk"
+
+    }, {
+        id: 10,
+        imgSrc: "/assets/imgs/banner-11.webp", title: "Mednza"
+
+    }, {
+        id: 11,
+        imgSrc: "/assets/imgs/banner-12.webp", title: "Zabon"
+
+    }, {
+        id: 12,
+        imgSrc: "/assets/imgs/banner-13.webp", title: "Bahaar"
+
+    }, {
+        id: 13,
+        imgSrc: "/assets/imgs/banner-14.webp", title: "Crepe & Waffle"
+
+    },
+    {
+        id: 14,
+        imgSrc: "/assets/imgs/banner-15.webp", title: "Al Yusr"
+
+    }
+];
+export default imgs

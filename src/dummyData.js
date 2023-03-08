ee40013bc11f0f5a9ac48d103ad07da7ed6c9193
AshrafElsaghier1 <ashrafelsaghier17@gmail.com>
2023-03-08 02:34:51 +0200

    fix imgSrc path

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,68 +1,68 @@
 const imgs = [
     {
         id: 1,
-        imgSrc: "assets/imgs/banner.webp",
+        imgSrc: "./assets/imgs/banner.webp",
         title: "Mercato"
     },
     {
         id: 2,
-        imgSrc: "/assets/imgs/banner-3.webp",
+        imgSrc: "./assets/imgs/banner-3.webp",
         title: "Cut"
     },
     {
         id: 3,
-        imgSrc: "/assets/imgs/banner-4.webp",
+        imgSrc: "./assets/imgs/banner-4.webp",
         title: "Eksel"
     },
 
     {
         id: 4,
-        imgSrc: "/assets/imgs/banner-5.webp", title: "Taakkad"
+        imgSrc: "./assets/imgs/banner-5.webp", title: "Taakkad"
     },
     {
         id: 5,
-        imgSrc: "/assets/imgs/banner-6.webp", title: "Shiftat"
+        imgSrc: "./assets/imgs/banner-6.webp", title: "Shiftat"
 
     },
     {
         id: 6,
-        imgSrc: "/assets/imgs/banner-7.webp", title: "Salonat"
+        imgSrc: "./assets/imgs/banner-7.webp", title: "Salonat"
 
     },
     {
         id: 7,
-        imgSrc: "/assets/imgs/banner-8.webp", title: "Kilinikat"
+        imgSrc: "./assets/imgs/banner-8.webp", title: "Kilinikat"
 
     }
 
     , {
         id: 8,
-        imgSrc: "/assets/imgs/banner-9.webp", title: "Basket"
+        imgSrc: "./assets/imgs/banner-9.webp", title: "Basket"
 
     }, {
         id: 9,
-        imgSrc: "/assets/imgs/banner-10.webp", title: "A R2yk"
+        imgSrc: "./assets/imgs/banner-10.webp", title: "A R2yk"
 
     }, {
         id: 10,
-        imgSrc: "/assets/imgs/banner-11.webp", title: "Mednza"
+        imgSrc: "./assets/imgs/banner-11.webp", title: "Mednza"
 
     }, {
         id: 11,
-        imgSrc: "/assets/imgs/banner-12.webp", title: "Zabon"
+        imgSrc: "./assets/imgs/banner-12.webp", title: "Zabon"
 
     }, {
         id: 12,
-        imgSrc: "/assets/imgs/banner-13.webp", title: "Bahaar"
+        imgSrc: "./assets/imgs/banner-13.webp", title: "Bahaar"
 
     }, {
         id: 13,
-        imgSrc: "/assets/imgs/banner-14.webp", title: "Crepe & Waffle"
+        imgSrc: "./assets/imgs/banner-14.webp", title: "Crepe & Waffle"
 
     },
     {
         id: 14,
-        imgSrc: "/assets/imgs/banner-15.webp", title: "Al Yusr"
+        imgSrc: "./assets/imgs/banner-15.webp", title: "Al Yusr"
 
     }
 ];
